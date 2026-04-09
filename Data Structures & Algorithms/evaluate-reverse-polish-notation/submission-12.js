6bbb283c0ccede2fc7e34d827505c665afb28a31
Ajit Kumar <55077889+Ajit-Kumar-1@users.noreply.github.com>
2026-04-09 20:25:51 +0100

    Add: evaluate-reverse-polish-notation - submission-12

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-12.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {string[]} tokens
+     * @return {number}
+     */
+    evalRPN(tokens) {
+        const vals = [];
+        tokens.forEach((t) => {
+            if (t === '+') {
+                vals.push(vals.pop() + vals.pop());
+            }
+            else if (t === '*') {
+                vals.push(vals.pop() * vals.pop());
+            }
+            else if (t === '-') {
+                const o1 = vals.pop();
+                const o2 = vals.pop();
+                vals.push(o2 - o1);
+            }
+            else if (t === '/') {
+                const o1 = vals.pop();
+                const o2 = vals.pop();
+                vals.push(Math.trunc(o2 / o1));
+            }
+            else vals.push(parseInt(t));
+            console.log(vals)
+        });
+        return vals[0];
+    }
+}
