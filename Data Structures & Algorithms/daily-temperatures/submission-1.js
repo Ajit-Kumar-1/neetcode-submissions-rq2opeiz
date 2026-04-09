0f4d5c7564d49f178ef4b128b8971324e7408795
Ajit Kumar <55077889+Ajit-Kumar-1@users.noreply.github.com>
2026-04-09 21:28:43 +0100

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} temperatures
+     * @return {number[]}
+     */
+    dailyTemperatures(temperatures) {
+        const res = new Array(temperatures.length).fill(0);
+        let stack = [];
+        for (let i = 0; i < temperatures.length; i++) {
+            const temp = temperatures[i];
+
+            while (stack.length > 0 && temp > stack[stack.length - 1][0]) {
+                const stackIndex = stack.pop()[1];
+                res[stackIndex] = i - stackIndex;
+            }
+            stack.push([temp, i]);
+        }
+        return res;
+    }
+}
