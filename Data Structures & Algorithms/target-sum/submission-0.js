cfeead9393ad868d3badd3d2b224c0afcacf36cb
Ajit Kumar <55077889+Ajit-Kumar-1@users.noreply.github.com>
2026-04-14 12:28:21 +0100

    Add: target-sum - submission-0

---
+++ Data Structures & Algorithms/target-sum/submission-0.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    findTargetSumWays(nums, target) {
+        const m = {};
+        let res = 0;
+
+        (function dfs(i = 0, sum = 0, vals = []){
+            if (i === nums.length) {
+                if (sum === target) res++;
+                return;
+            }
+            const num = nums[i];
+            dfs(i + 1, sum - num, [...vals, -num]);
+            dfs(i + 1, sum + num, [...vals, num]);
+        })();
+
+        return res;
+    }
+}
