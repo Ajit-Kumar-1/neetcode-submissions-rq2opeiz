96741e2fde7485c223aee31ca8b36892b0d2ade3
Ajit Kumar <55077889+Ajit-Kumar-1@users.noreply.github.com>
2026-05-16 14:37:43 +0100

    Add: asteroid-collision - submission-0

---
+++ Data Structures & Algorithms/asteroid-collision/submission-0.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} asteroids
+     * @return {number[]}
+     */
+    asteroidCollision(asteroids) {
+        const stack = [];
+        for (let a of asteroids) {
+            while (stack.length && stack[stack.length - 1] > 0 && a < 0) {
+                const diff = a + stack[stack.length - 1];
+                if (diff > 0) a = 0;
+                else if (diff < 0) stack.pop();
+                else {
+                    a = 0;
+                    stack.pop();
+                }
+            }
+            if (a !== 0) stack.push(a);
+        }
+        return stack;
+    }
+}
